4bde26eefc7d391b06b9ef65e97be495bd746165
Andrew Hobden <andrew@hoverbear.org>
2016-02-27 15:08:09 -0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# The Infinite Lyricist
+
+A CSC-475 at the University of Victoria project by Andrew Hobden, Brody Holden, and Pascale Mendes.
+
+This is a work in progress. Full instructions will follow.
+
+## Prerequisites
+
+You must be using Python 2.7 (Not 3.0) and have a valid Marsyas installation with Swig bindings enabled in the build.
+
+Then you can just run `./infinite-lyricist vocal.mp3 instrumental.mp3`.
---
+++ infinite-lyricist.py
@@ -0,0 +1,17 @@
+#! /bin/env python2.7
+import marsyas
+import argparse
+
+# First, let's parse the command line options.
+parser = argparse.ArgumentParser(description='Mashes a vocal track onto a instrumental track.')
+parser.add_argument('vocal_input', metavar='vocal_input', help='The vocal input')
+parser.add_argument('instrumental_input', metavar='instrumental_input', help='The instrumental input')
+
+# Now we parse them and pick up the existing files or error out.
+args = parser.parse_args()
+vocal_track = open(args.vocal_input)
+instrumental_track = open(args.instrumental_input)
+
+# Debug
+print(args)
+
