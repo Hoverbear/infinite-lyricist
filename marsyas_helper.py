e5d81a9aaf32dc12e950c87fe0e38de1099859f3
Andrew Hobden <andrew@hoverbear.org>
2016-04-04 17:22:26 -0700

    KEy detection that doesn't suck as bad

--- marsyas_helper.py
+++ marsyas_helper.py
@@ -21,31 +21,30 @@ def detect_key(music_file):
     """
     This function assists in running Marsyas scripts.
     """
-    keys = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"] 
+    from collections import Counter
+
+    keys = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
     # Build the system.
     msm = marsyas.MarSystemManager()
     system = marsyas.system_from_script_file("marsystems/key-detection.mrs")
     # Define some variables
     system.updControl("mrs_string/file", marsyas.MarControlPtr.from_string(music_file))
-    
+
     result = []
     # Tick the system.
     while (system.getControl("SoundFileSource/input/mrs_bool/hasData").to_bool()):
         system.tick()
-        result.append(list(system.getControl("mrs_realvec/output").to_realvec()))
-    
-    instance_count = [0]*12
-    for line in result:
-        for index,value in enumerate(line):
-            if value == 1:
-                instance_count[index] += 1
-    print(instance_count) 
-    return keys[instance_count.index(max(instance_count))]
+        result.append(system.getControl("mrs_string/key_name").to_string())
+
+    counts = Counter(result).most_common();
+    print(counts)
+    return counts[0][0]
+
 
 if __name__ == "__main__":
-    selection = detect_key("samples/aminor.mp3")
-    print(selection)
-    selection = detect_key("samples/asharp.mp3")
-    print(selection)
-    selection = detect_key("samples/fmajor.mp3")
-    print(selection)
+    selection = detect_key("song/wav/song-synth.wav")
+    print("Truth: e, Chosen: " + selection)
+    selection = detect_key("song/wav/song-bass.wav")
+    print("Truth: a, Chosen: " + selection)
+    selection = detect_key("song/wav/song-guitar.wav")
+    print("Truth: a, Chosen: " + selection)
