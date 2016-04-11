bdbf6e6058c8fcbb2c8f8365b699eebd02abbfff
pascale-m <lilp@Pascales-MacBook-Pro.local>
2016-04-10 19:44:53 -0700

    pitch-shift implemented

--- phase_vocoder.py
+++ phase_vocoder.py
@@ -1,21 +1,22 @@
 import marsyas
 
-oscbank_ = False
-N = 512
-Nw = 512
-D = 64
-I = 64
+N = 1024
+Nw = 1024
+D = 16
+I = 16
 P = 1.0
 unconvertmode_ = "classic"
 convertmode_ = "sorted"
 multires_ = False
 multiresMode_ = "transient_switch"
-bopt = 128
+bopt = 320
 sopt = 80
 music_file = ""
-options = {'music_file':music_file, 'N':N, 'Nw':Nw, 'D':D, 'I':I, 'P':P, 'unconvertmode_':unconvertmode_, 'convertmode_':convertmode_, 'multires_':multires_, 'multiresMode_':multiresMode_, 'bopt':bopt, 'sopt':sopt, 'oscbank_':oscbank_}
+options = {'music_file':music_file, 'N':N, 'Nw':Nw, 'D':D, 'I':I, 'P':P, 'unconvertmode_':unconvertmode_, 'convertmode_':convertmode_, 'multires_':multires_, 'multiresMode_':multiresMode_, 'bopt':bopt, 'sopt':sopt}
 
 def time_shift(**args):
+
+	outfile = "./pv/pv_output_phaselock.wav"
 
 	for arg in args:
 		options[arg] = args[arg]
@@ -26,7 +27,7 @@ def time_shift(**args):
 	pvseries = marsyas.system_from_script_file("./marsystems/time-shift.mrs")
 
 	pvseries.updControl("mrs_string/file", options['music_file'])
-	pvseries.updControl("mrs_string/outfile", "./pv/pv_output_phaselock.wav")
+	pvseries.updControl("mrs_string/outfile", outfile)
 
 
   	pvseries.updControl("mrs_real/israte", 44100.0)
@@ -75,7 +76,7 @@ def time_shift(**args):
 	pvseries.updControl("PvUnconvert/uconv/mrs_string/mode",options['unconvertmode_'])
 
 	pvseries.updControl("InvSpectrum/ispectrum/mrs_natural/onObservations", 1)
-	pvseries.updControl("InvSpectrum/ispectrum/mrs_natural/onSamples", 1024)
+	pvseries.updControl("InvSpectrum/ispectrum/mrs_natural/onSamples", options['N'])
 
 	pvseries.updControl("PvOverlapadd/pover/mrs_natural/FFTSize", options['N'])
 	pvseries.updControl("PvOverlapadd/pover/mrs_natural/winSize", options['Nw'])
@@ -100,17 +101,93 @@ def time_shift(**args):
 		ticks = ticks + 1
 		print ticks*(options['D']/44100.0)
 
-	return "pv_output.wav"
+	return outfile
+
+
+def pitch_shift(**args):
+	outfile = "./pv/pitch_shift1.wav"
+
+	for arg in args:
+		options[arg] = args[arg]
+
+	print options
+
+	mng = marsyas.MarSystemManager()
+	pvseries = marsyas.system_from_script_file("./marsystems/pitch-shift.mrs")
+
+	pvseries.updControl("mrs_string/file", options['music_file'])
+	pvseries.updControl("mrs_string/outfile", outfile)
+
+
+  	pvseries.updControl("mrs_real/israte", 44100.0)
+	pvseries.updControl("mrs_real/osrate", 44100.0)
+	pvseries.updControl("mrs_natural/inSamples", options['D'])
+	pvseries.updControl("mrs_natural/onSamples", options['I'])
+	pvseries.updControl("mrs_natural/onStabilizingDelay", 7)
+
+	pvseries.updControl("SoundFileSource/src/mrs_natural/onObservations", 2)
+
+	pvseries.updControl("ShiftInput/si/mrs_natural/winSize", options['Nw'])
+	pvseries.updControl("ShiftInput/si/mrs_natural/onSamples", options['N'])
+	
+
+
+	pvseries.updControl("PvFold/fo/mrs_natural/FFTSize", options['N'])
+	pvseries.updControl("PvFold/fo/mrs_natural/onObservations", 1)
+
+	pvseries.updControl("Spectrum/spk/mrs_natural/onObservations", options['N'])
+	pvseries.updControl("Spectrum/spk/mrs_natural/onSamples", 1)
+
+	pvseries.updControl("PvConvert/conv/mrs_natural/Sinusoids", options['sopt'])
+	pvseries.updControl("PvConvert/conv/mrs_natural/Decimation", options['D'])
+
+	pvseries.updControl("PvOscBank/osc/mrs_real/PitchShift", options['P'])
+	pvseries.updControl("PvOscBank/osc/mrs_natural/winSize", options['Nw'])
+	pvseries.updControl("PvOscBank/osc/mrs_natural/Interpolation", options['I'])
+	pvseries.updControl("PvOscBank/osc/mrs_natural/onSamples", options['N'])
+
+	pvseries.linkControl("PvConvert/conv/mrs_realvec/phases", "PvOscBank/osc/mrs_realvec/analysisphases")
+
+	pvseries.updControl("ShiftOutput/so/mrs_natural/Interpolation", options['I'])
+
+	pvseries.updControl("SoundFileSink/last/mrs_natural/bitrate", 320)
+
+
+
+	ticks = 0
+
+	notempty = pvseries.getControl("SoundFileSource/src/mrs_bool/hasData")
+
+	while (notempty.to_bool()):
+		if(ticks == 0):
+			pass
+			#pvseries.updControl("PvOscBank/osc/mrs_bool/phaselock", marsyas.MarControlPtr.from_bool(True))
+
+		pvseries.tick()
+		print ticks
+		ticks = ticks + 1
+		print ticks*(options['D']/44100.0)
+
+	return outfile
 
 
 
 
 if __name__ == "__main__":
 
-	speed = 2
+	"""speed = 2
 	ipol = int(16*speed)
 	infile = "../../phasevocoding/nolove.wav"
 	print "Output file will be at", speed, "times original speed."
 	outputfile = time_shift(N=1024, Nw=1024, D=16, I=ipol, music_file=infile)
+	print("Done!")"""
+
+	infile = "../../phasevocoding/nolove.wav"
+	pitch = 1.5
+	print "Output file will be at", pitch, "times original pitch"
+	outputfile = pitch_shift(P=pitch, sopt=220, music_file=infile)
+	print outputfile
 	print("Done!")
+
+
 
