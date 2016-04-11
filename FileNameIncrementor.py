fe3cb5479bc142b1a268f256eee18a5ff8ec726a
Andrew Hobden <andrew@hoverbear.org>
2016-04-10 21:10:49 -0700

    Documented FileNameINcrementor

--- FileNameIncrementor.py
+++ FileNameIncrementor.py
@@ -2,9 +2,16 @@ import os
 
 
 class FileNameIncrementor:
-
+    """
+    Provides an automated way to increment file names.
+    """
 
     def __init__(self, filename):
+        """
+        Creates an incrementor.
+
+            * filename: The root of the filename you wish to increment.
+        """
         parts = os.path.splitext(str(filename))
         self.__root_name = parts[0]
         self.__ext = parts[1]
@@ -12,16 +19,25 @@ class FileNameIncrementor:
 
 
     def next(self):
+        """
+        Returns the next file name after incrementing.
+        """
         name = self.__root_name + str(self.__count) + self.__ext
         self.__count += 1
         return name
 
 
     def root_name(self):
+        """
+        Returns the root name.
+        """
         return self.__root_name
 
 
     def ext(self):
+        """
+        Returns the file extension.
+        """
         return self.__ext
 
 
@@ -35,5 +51,3 @@ if __name__ == "__main__":
 
     print "root name =", fni.root_name()
     print "ext =", fni.ext()
-
-
